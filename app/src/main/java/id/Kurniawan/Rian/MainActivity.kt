ec4f3b25940d2fd282cd5dfbef3ed5bb19080e9c
TaatAmrullah5 <195410054@students.akakom.ac.id>
2021-10-29 19:43:32 +0700

    Mobile

---
+++ app/src/main/java/id/Kurniawan/Rian/MainActivity.kt
@@ -0,0 +1,63 @@
+package id.Kurniawan.Rian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.submit)
+        btn.setOnClickListener{simulasi()}
+    }
+
+    private fun currencyConvert(num: Double): String {
+        return NumberFormat.getCurrencyInstance().format(num)
+    }
+
+    private fun simulasi(){
+
+        // Get All result layout element
+        val biaya: LinearLayout = findViewById(R.id.layanan)
+        val bungapinjam: LinearLayout = findViewById(R.id.bunga)
+        val totalcair: LinearLayout = findViewById(R.id.pencairan)
+        val totaluang: LinearLayout = findViewById(R.id.total)
+
+        // Get Input Value
+        val inputpinjaman: TextInputEditText = findViewById(R.id.pinjamedit)
+        val month: TextInputEditText = findViewById(R.id.monthedit)
+        var pinjaman = inputpinjaman.text.toString().toDoubleOrNull()
+        var monthvalue = month.text.toString().toDoubleOrNull()
+
+        // Calculate Loan Simulation
+        var layanan = pinjaman?.times(0.05)
+        var bunga = pinjaman?.times(0.0375)
+        var pencairan= pinjaman?.minus(layanan!!)
+        var total = monthvalue?.let { bunga?.times(it)?.let { pinjaman?.plus(it) } }
+
+        // Get Result Element
+        val biaya_res: TextView = findViewById(R.id.biayalayan1)
+        val bunga_res: TextView = findViewById(R.id.bunga2)
+        val totalcair_res: TextView = findViewById(R.id.totalcair2)
+        val total_res: TextView = findViewById(R.id.total2)
+
+        // put to text
+        biaya_res.text = currencyConvert(layanan!!)
+        bunga_res.text = currencyConvert(bunga!!)
+        totalcair_res.text = currencyConvert(pencairan!!)
+        total_res.text = currencyConvert(total!!)
+
+        // Make is Visible
+        biaya.visibility = View.VISIBLE
+        bungapinjam.visibility = View.VISIBLE
+        totalcair.visibility = View.VISIBLE
+        totaluang.visibility = View.VISIBLE
+    }
+}
